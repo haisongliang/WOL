e6ff5c70e9f320181d3d0d0ec36c11cc68ea0232
haisongliang <haisongliang@gmail.com>
2019-06-29 22:35:27 +0800

    add code

---
+++ ARP/ARP.cpp
@@ -0,0 +1,95 @@
+﻿// ARP.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <iostream>
+#include <WinSock2.h>
+#include <windows.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+int main()
+{
+	DWORD dwRetVal;
+	IPAddr DestIp = 0;
+	IPAddr SrcIp = 0;
+	ULONG MacAddr[2];       /* for 6-byte hardware addresses */
+	ULONG PhyAddrLen = 6;  /* default to length of six bytes */
+
+	BYTE* bPhysAddr;
+
+	char ip_buff[64] = "";
+	for (int ip = 99; ip < 254; ip++)
+	{
+		memset(ip_buff, 0, sizeof(ip_buff));
+		sprintf_s(ip_buff, "192.168.1.%d", ip + 1);
+		DestIp = inet_addr(ip_buff);
+
+		memset(&MacAddr, 0xff, sizeof(MacAddr));
+
+		printf("Sending ARP request for IP address: %s\n", ip_buff);
+
+		dwRetVal = SendARP(DestIp, SrcIp, &MacAddr, &PhyAddrLen);
+
+		if (dwRetVal == NO_ERROR) {
+			bPhysAddr = (BYTE*)& MacAddr;
+			if (PhyAddrLen) {
+				for (int i = 0; i < (int)PhyAddrLen; i++) 
+				{
+					if (i == (PhyAddrLen - 1))
+						printf("%.2X\n", (int)bPhysAddr[i]);
+					else
+						printf("%.2X-", (int)bPhysAddr[i]);
+				}
+			}
+			else
+				printf
+				("Warning: SendArp completed successfully, but returned length=0\n");
+
+		}
+		else {
+			printf("Error: SendArp failed with error: %d", dwRetVal);
+			switch (dwRetVal) {
+			case ERROR_GEN_FAILURE:
+				printf(" (ERROR_GEN_FAILURE)\n");
+				break;
+			case ERROR_INVALID_PARAMETER:
+				printf(" (ERROR_INVALID_PARAMETER)\n");
+				break;
+			case ERROR_INVALID_USER_BUFFER:
+				printf(" (ERROR_INVALID_USER_BUFFER)\n");
+				break;
+			case ERROR_BAD_NET_NAME:
+				printf(" (ERROR_GEN_FAILURE)\n");
+				break;
+			case ERROR_BUFFER_OVERFLOW:
+				printf(" (ERROR_BUFFER_OVERFLOW)\n");
+				break;
+			case ERROR_NOT_FOUND:
+				printf(" (ERROR_NOT_FOUND)\n");
+				break;
+			default:
+				printf("\n");
+				break;
+			}
+		}
+	}
+
+	system("pause");
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ WOL/WOL.cpp
@@ -0,0 +1,102 @@
+﻿// WOL.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WinSock2.h>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+#pragma comment(lib,"Ws2_32.lib")
+
+int main()
+{
+	//初始化网络
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	//创建socket
+	SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		fprintf(stderr, "Socket create error: %d\n", GetLastError());
+		return 0;
+	}
+
+	//设置为广播发送
+	BOOL bOptVal = TRUE;
+	int iOptLen = sizeof(BOOL);
+	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)& bOptVal, iOptLen) == SOCKET_ERROR)
+	{
+		fprintf(stderr, "setsockopt error: %d\n", WSAGetLastError());
+		closesocket(sock);
+		WSACleanup();
+		return 0;
+	}
+
+	sockaddr_in to;
+	to.sin_family = AF_INET;
+	to.sin_port = htons(0);
+	to.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
+	vector<string> macs;
+	ifstream ifs("macs.txt");
+	string macStr;
+	if (ifs)
+	{
+		while (!ifs.eof())
+		{
+			ifs >> macStr >> macStr;
+			macs.push_back(macStr);
+		}
+	}
+
+	for (int jj = 0; jj < macs.size(); jj++)
+	{
+		const char* mac = macs[jj].c_str();
+		int dstaddr[6];
+		int i = sscanf_s(mac, "%2x-%2x-%2x-%2x-%2x-%2x",
+			&dstaddr[0], &dstaddr[1], &dstaddr[2], &dstaddr[3], &dstaddr[4], &dstaddr[5]);
+		if (i != 6)
+		{
+			fprintf(stderr, "Invalid MAC Adresse!");
+			return 0;
+		}
+
+		unsigned char ether_addr[6];
+		for (i = 0; i < 6; i++)
+			ether_addr[i] = dstaddr[i];
+
+		//构造Magic Packet
+		u_char magicpacket[200];
+		memset(magicpacket, 0xff, 6);
+		int packetsize = 6;
+		for (i = 0; i < 16; i++)
+		{
+			memcpy(magicpacket + packetsize, ether_addr, 6);
+			packetsize += 6;
+		}
+		//发送Magic Packet
+		if (sendto(sock, (const char*)magicpacket, packetsize, 0, (const struct sockaddr*) & to, sizeof(to)) == SOCKET_ERROR)
+			fprintf(stderr, "Magic packet send error: %d\n", WSAGetLastError());
+		else
+			printf("Magic packet send!\n");
+	}
+
+	closesocket(sock);
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
